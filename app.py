63133e620e760a8e19b85dc4b31243b80cdca132
kajackdfw <kajackdfw@yahoo.com>
2016-12-14 16:35:12 -0600

    cloned flask_garden

---
+++ app.py
@@ -0,0 +1,23 @@
+from flask import Flask, render_template
+from os import listdir
+from os.path import isfile, join
+import os
+
+app = Flask(__name__)
+server_os = os.name
+print(" * System OS is " + os.name)
+
+@app.route('/')
+def index():
+    mypath = 'static/cam'
+    imagefiles = [f for f in listdir(mypath) if isfile(join(mypath, f))]
+    return render_template('index.html', images=imagefiles)
+
+@app.route('/view/<image>')
+def view(image):
+    return render_template('view.html', image=image)
+
+if __name__ == '__main__' and os.name == 'posix':
+    app.run(debug=True, host='0.0.0.0')
+elif __name__ == '__main__':
+    app.run(debug=True, host='localhost')
