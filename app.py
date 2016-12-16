7120dd74d32986e12069c58367e9710e1a19f805
kajackdfw <kajackdfw@yahoo.com>
2016-12-16 09:42:12 -0600

    buttons over image

--- app.py
+++ app.py
@@ -6,16 +6,22 @@ import os
 app = Flask(__name__)
 server_os = os.name
 print(" * System OS is " + os.name)
+mypath = 'static/cam'
 
 @app.route('/')
 def index():
-    mypath = 'static/cam'
+    # mypath = 'static/cam'
     imagefiles = [f for f in listdir(mypath) if isfile(join(mypath, f))]
     return render_template('index.html', images=imagefiles)
 
 @app.route('/view/<image>')
 def view(image):
     return render_template('view.html', image=image)
+
+@app.route('/drive')
+def drive():
+    imagefiles = [f for f in listdir(mypath) if isfile(join(mypath, f))]
+    return render_template('drive.html', images=imagefiles)
 
 if __name__ == '__main__' and os.name == 'posix':
     app.run(debug=True, host='0.0.0.0')
