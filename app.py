10339f7e4d017cc470d3343b130fa8598c5bebeb
kajackdfw <kajackdfw@yahoo.com>
2017-01-13 16:26:49 -0600

    cropped_image

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template
 import os
 import sys
+from PIL import Image
 
 sys.path.append('hardware_drivers/vision')
 from raspberry_pi_8mp import Vision  # change raspberry_pi_8mp to match your hardware
@@ -37,6 +38,23 @@ def index():
 def view(image):
     path = vision.settings['path_to_pictures']
     return render_template('view.html', image=image, path=path)
+
+
+@app.route('/get/<image_file>/<zoom_in>/<x>/<y>')
+def get_image(image_file,zoom_in,x,y):
+    path = vision.settings['path_to_pictures']
+    print('open ' + path + '/' + image_file)
+    temp_image = Image.open(path + '/' + image_file)
+    print('format:' + str(temp_image.format))
+    print('size  : ' + str(temp_image.size))
+    print('mode  : ' + str(temp_image.mode))
+
+    # What will we call this new image
+    file_name, extension = os.path.splitext(image_file)
+    output_file = 'static/tmp/cropped_image.' + extension
+    print('save to ' + output_file)
+    temp_image.save(output_file)
+    return 'success'
 
 
 @app.route('/drive')
