4e184efdf609df4c549fda8e2aa5c27dc27dbfda
kajackdfw <kajackdfw@yahoo.com>
2017-01-18 10:34:52 -0600

    immobile option

---
+++ hardware_drivers/motor/immobile_wildlife_cam.py
@@ -0,0 +1,9 @@
+# no motors, this is a stationary rover or wildlife cam
+
+class Motor:
+
+    settings = {}
+
+    def __init__(self, start_settings):
+        self.settings['drive_mode'] = 'immobile'
+        self.can_rotate_in_place = False
