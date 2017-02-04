6dc63300d741ca74d91bf1cd9cf2c5fd60904c41
Kelly Taylor <kajackdfw@yahoo.com>
2017-02-03 22:36:36 -0600

    ui work

---
+++ templates/partials/common_button_methods.js
@@ -0,0 +1,14 @@
+
+        $("#sm_home_page").click(function(){ window.location.href = "/"; });
+        $("#sm_drive_page").click(function(){ window.location.href = "/drive"; });
+        $("#sm_pictures_page").click(function(){ window.location.href = "/pictures"; });
+        $("#sm_about_page").click(function(){ window.location.href = "/about"; });
+
+        $("#lg_home_page").click(function(){ window.location.href = "/"; });
+        $("#lg_drive_page").click(function(){ window.location.href = "/drive"; });
+        $("#lg_pictures_page").click(function(){ window.location.href = "/pictures"; });
+        $("#lg_about_page").click(function(){ window.location.href = "/about"; });
+
+        $("#sm_take_picture").click(function(){ take_picture(); });
+
+        $("#lg_take_picture").click(function(){ take_picture(); });
