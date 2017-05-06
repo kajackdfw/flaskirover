3c3494500b5bb06bad437411322ce722a40d2a10
Kelly Taylor <kajackdfw@yahoo.com>
2017-05-05 20:55:08 -0500

    Merge branch 'master' of https://github.com/kajackdfw/python_flask_rover_ui
    # Conflicts:
    #	hardware_drivers/motor/adafruit_dc_and_stepper_motor_hat.py

--- hardware_drivers/vision/servos_on_gpio.py
+++ hardware_drivers/vision/servos_on_gpio.py
@@ -20,29 +20,25 @@ class Gimbal:
         uis['gimbal_vert'] = 'disabled'
 
     def __init__(self, start_settings):
-        self.settings['gimbal_horz'] = start_settings['gimbal_horz_servo_gpio']
-        self.settings['gimbal_vert'] = start_settings['gimbal_vert_servo_gpio']
+        self.settings['gimbal_horz_servo_gpio'] = start_settings['gimbal_horz_servo_gpio']
+        self.settings['gimbal_vert_servo_gpio'] = start_settings['gimbal_vert_servo_gpio']
 
-        if not self.settings['gimbal_horz'] == 'False':
-            self.settings['gimbal_horz'] = int(self.settings['gimbal_horz'])
-            self.settings['gimbal_horz_full_down'] = start_settings['gimbal_horz_full_down']
+        if (self.settings['gimbal_horz_servo_gpio']) is not False:
+            self.settings['gimbal_horz_full_left'] = start_settings['gimbal_horz_full_left']
             self.settings['gimbal_horz_center'] = start_settings['gimbal_horz_center']
-            self.settings['gimbal_horz_full_up'] = start_settings['gimbal_horz_full_up']
+            self.settings['gimbal_horz_full_right'] = start_settings['gimbal_horz_full_right']
             self.settings['gimbal_horz_step'] = start_settings['gimbal_horz_step']
             self.uis['gimbal_horz'] = 'active'
         else:
-            self.settings['gimbal_horz'] = False
             self.uis['gimbal_horz'] = 'disabled'
 
-        if not self.settings['gimbal_vert'] == 'False':
-            self.settings['gimbal_vert'] = int(self.settings['gimbal_vert'])
+        if (self.settings['gimbal_vert_servo_gpio']) is not False:
             self.settings['gimbal_vert_full_down'] = start_settings['gimbal_vert_full_down']
-            self.settings['gimbal_vert_center'] = start_settings['gimbal_vert_full_down']
-            self.settings['gimbal_vert_full_up'] = start_settings['gimbal_vert_full_down']
+            self.settings['gimbal_vert_center'] = start_settings['gimbal_vert_center']
+            self.settings['gimbal_vert_full_up'] = start_settings['gimbal_vert_full_up']
             self.settings['gimbal_vert_step'] = start_settings['gimbal_vert_step']
             self.uis['gimbal_vert'] = 'active'
         else:
-            self.settings['gimbal_vert'] = False
             self.uis['gimbal_vert'] = 'disabled'
 
     def get_uis(self):
