ccaa4cd39fa8e19e3b9c445b754cf97c51880678
kajackdfw <kajackdfw@yahoo.com>
2017-05-11 13:31:22 -0500

    added wiringpi code

--- hardware_drivers/gimbal/servos_on_gpio.py
+++ hardware_drivers/gimbal/servos_on_gpio.py
@@ -1,8 +1,8 @@
 try:
-    import time
     import wiringpi
 except ImportError:
     print(' - Library wiringpi is not available on Windows.')
+
 
 class Gimbal:
 
@@ -13,10 +13,6 @@ class Gimbal:
 
     try:
         wiringpi.wiringPiSetupGpio()
-        wiringpi.pinMode(18, wiringpi.GPIO.PWM_OUTPUT)
-        wiringpi.pwmSetMode(wiringpi.GPIO.PWM_MODE_MS)
-        wiringpi.pwmSetClock(192)
-        wiringpi.pwmSetRange(2000)
         delay_period = 0.01
         uis['gimbal_horz'] = 'active'
         uis['gimbal_vert'] = 'active'
@@ -30,52 +26,71 @@ class Gimbal:
         self.settings['gimbal_vert_servo_gpio'] = start_settings['gimbal_vert_servo_gpio']
 
         if (self.settings['gimbal_horz_servo_gpio']) is not False and self.uis['gimbal_horz'] is not 'disabled':
-            self.settings['gimbal_horz_full_left'] = start_settings['gimbal_horz_full_left']
-            self.settings['gimbal_horz_center'] = start_settings['gimbal_horz_center']
-            self.settings['gimbal_horz_full_right'] = start_settings['gimbal_horz_full_right']
-            self.settings['gimbal_horz_step'] = start_settings['gimbal_horz_step']
+            wiringpi.pinMode(self.settings['gimbal_horz_servo_gpio'], wiringpi.GPIO.PWM_OUTPUT)
+            self.settings['gimbal_horz_servo_gpio'] = int(self.settings['gimbal_horz_servo_gpio'])
+            self.settings['gimbal_horz_full_left'] = int(start_settings['gimbal_horz_full_left'])
+            self.settings['gimbal_horz_center'] = int(start_settings['gimbal_horz_center'])
+            self.settings['gimbal_horz_position'] = int(start_settings['gimbal_horz_center'])
+            self.settings['gimbal_horz_full_right'] = int(start_settings['gimbal_horz_full_right'])
+            self.settings['gimbal_horz_step'] = int(start_settings['gimbal_horz_step'])
         else:
             self.uis['gimbal_horz'] = 'disabled'
 
         if (self.settings['gimbal_vert_servo_gpio']) is not False and self.uis['gimbal_vert'] is not 'disabled':
-            self.settings['gimbal_vert_full_down'] = start_settings['gimbal_vert_full_down']
-            self.settings['gimbal_vert_center'] = start_settings['gimbal_vert_center']
-            self.settings['gimbal_vert_full_up'] = start_settings['gimbal_vert_full_up']
-            self.settings['gimbal_vert_step'] = start_settings['gimbal_vert_step']
+            wiringpi.pinMode(self.settings['gimbal_vert_servo_gpio'], wiringpi.GPIO.PWM_OUTPUT)
+
+            self.settings['gimbal_vert_servo_gpio'] = int(self.settings['gimbal_vert_servo_gpio'])
+            self.settings['gimbal_vert_full_down'] = int(start_settings['gimbal_vert_full_down'])
+            self.settings['gimbal_vert_center'] = int(start_settings['gimbal_vert_center'])
+            self.settings['gimbal_vert_position'] = int(start_settings['gimbal_vert_center'])
+            self.settings['gimbal_vert_full_up'] = int(start_settings['gimbal_vert_full_up'])
+            self.settings['gimbal_vert_step'] = int(start_settings['gimbal_vert_step'])
         else:
             self.uis['gimbal_vert'] = 'disabled'
 
-        # Initialize servos
-        if self.settings['gimbal_vert_servo_gpio'] and self.uis['gimbal_vert'] == 'active':
-            GPIO.setup(self.settings['gimbal_vert_servo_gpio'], GPIO.OUT)
-            self.pwm_vert = GPIO.PWM(self.settings['gimbal_vert_servo_gpio'], 50)
-            self.pwm_vert.start(5)
-            self.center()
-
-        if self.settings['gimbal_horz_servo_gpio'] and self.uis['gimbal_horz'] == 'active':
-            GPIO.setup(self.settings['gimbal_horz_servo_gpio'], GPIO.OUT)
-            self.pwm_horz= GPIO.PWM(self.settings['gimbal_horz_servo_gpio'], 50)
-            self.pwm_horz.start(5)
+        # Initialize PWM and first servo positions
+        if self.uis['gimbal_vert'] == 'active' or self.uis['gimbal_horz'] == 'active':
+            wiringpi.pwmSetMode(wiringpi.GPIO.PWM_MODE_MS)
+            wiringpi.pwmSetClock(192)
+            wiringpi.pwmSetRange(2000)
             self.center()
 
     def center(self):
-        if self.settings['gimbal_vert_servo_gpio']:
-            self.pwm_vert.ChangeDutyCycle(7.5)
-        if self.settings['gimbal_horz_servo_gpio']:
-            self.pwm_horz.ChangeDutyCycle(7.5)
+        if self.settings['gimbal_vert_servo_gpio'] is not False:
+            wiringpi.pwmWrite(self.settings['gimbal_vert_servo_gpio'], self.settings['gimbal_vert_center'])
+
+        if self.settings['gimbal_horz_servo_gpio'] is not False:
+            wiringpi.pwmWrite(self.settings['gimbal_horz_servo_gpio'], self.settings['gimbal_horz_center'])
+
         return True
 
     def rotate_down(self, degrees):
-        self.pwm_vert.ChangeDutyCycle(7)
+        self.settings['gimbal_vert_position'] -= self.settings['gimbal_vert_step'] * int(degrees)
+        if self.settings['gimbal_vert_position'] < self.settings['gimbal_vert_full_down']:
+            self.settings['gimbal_vert_position'] = self.settings['gimbal_vert_full_down']
+        wiringpi.pwmWrite(self.settings['gimbal_vert_servo_gpio'], self.settings['gimbal_vert_position'])
+        return False
 
     def rotate_up(self, degrees):
-        self.pwm_vert.ChangeDutyCycle(8)
+        self.settings['gimbal_vert_position'] += self.settings['gimbal_vert_step'] * int(degrees)
+        if self.settings['gimbal_vert_position'] > self.settings['gimbal_vert_full_up']:
+            self.settings['gimbal_vert_position'] = self.settings['gimbal_vert_full_up']
+        wiringpi.pwmWrite(self.settings['gimbal_vert_servo_gpio'], self.settings['gimbal_vert_position'])
+        return False
 
     def rotate_left(self, degrees):
-        self.pwm_horz.ChangeDutyCycle(7)
+        self.settings['gimbal_horz_position'] += self.settings['gimbal_horz_step'] * int(degrees)
+        if self.settings['gimbal_horz_position'] > self.settings['gimbal_horz_full_left']:
+            self.settings['gimbal_horz_position'] = self.settings['gimbal_horz_full_left']
+        wiringpi.pwmWrite(self.settings['gimbal_horz_servo_gpio'], self.settings['gimbal_horz_position'])
+        return False
 
     def rotate_right(self, degrees):
-        self.pwm_horz.ChangeDutyCycle(8)
+        self.settings['gimbal_horz_position'] -= self.settings['gimbal_horz_step'] * int(degrees)
+        if self.settings['gimbal_horz_position'] > self.settings['gimbal_horz_full_right']:
+            self.settings['gimbal_horz_position'] = self.settings['gimbal_horz_full_right']
+        wiringpi.pwmWrite(self.settings['gimbal_horz_servo_gpio'], self.settings['gimbal_horz_position'])
+        return False
 
     def get_uis(self):
         return self.uis
